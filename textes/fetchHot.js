af4ff10b42601af8aa3fe797b9a5e022af585ff7
ChiaLingHsu <h200022923@gmail.com>
2025-06-07 23:01:05 +0800

    加上對應英雄的網址

--- textes/fetchHot.js
+++ textes/fetchHot.js
@@ -18,10 +18,18 @@ export default async event => {
         const champId = className.split('-')[1]
         const engName = championMap[champId]
 
+        // 英雄圖片
         bubble.hero.url = `https://ddragon.leagueoflegends.com/cdn/14.10.1/img/champion/${engName}.png`
+
+        // 中文名稱
         bubble.body.contents[0].text = $(this).find('td').eq(1).find('.name').text().trim()
+
+        // 使用率
         bubble.body.contents[1].contents[0].contents[0].text =
           '使用率：' + (parseFloat($(this).find('td').eq(2).find('progressbar').attr('data-value')) * 100).toFixed(1) + '%'
+
+        // 對應英雄網址
+        bubble.footer.contents[0].action.uri = `https://wildrift.leagueoflegends.com/zh-tw/champions/${engName.toLowerCase()}/`
 
         character.push(bubble)
       }
