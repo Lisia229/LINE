7e1f88c050ba9fcf2f850e91379ea66dd412a890
ChiaLingHsu <h200022923@gmail.com>
2025-06-08 11:16:50 +0800

    Update fetchNews.js

--- textes/fetchNews.js
+++ textes/fetchNews.js
@@ -5,43 +5,29 @@ import writejson from '../utils/writejson.js'
 
 export default async event => {
   try {
-    const { data } = await axios.get('https://wildrift.leagueoflegends.com/zh-tw/news/game-updates/')
+    const { data } = await axios.get('https://wildrift.leagueoflegends.com/zh-tw/news/')
     const $ = cheerio.load(data)
     const news = []
-
-    $('.NewsList-module--newsList-- a').each(function (i) {
+    $('.sc-362cdf8e-0').each(function (i) {
       if (i >= 12) return false
-
       const bubble = JSON.parse(JSON.stringify(templates))
-      const href = $(this).attr('href') || ''
-      const imgSrc = $(this).find('img').attr('src') || ''
-      const imgAlt = $(this).find('img').attr('alt') || '激鬥峽谷新聞'
-      const dateText = $(this).find('time').text().trim() || ''
-
-      bubble.hero.url = imgSrc
-      bubble.body.contents[0].text = imgAlt
-      bubble.body.contents[1].contents[0].contents[0].text = '日期：' + dateText
-      bubble.footer.contents[0].action.uri = 'https://wildrift.leagueoflegends.com' + href
-
+      bubble.hero.url = $(this).find('img').attr('src')
+      bubble.body.contents[0].text = $(this).find('img').attr('alt')
+      bubble.body.contents[1].contents[0].contents[0].text = '日期：' + $(this).find('.copy-01').eq(0).text()
+      bubble.footer.contents[0].action.uri = 'https://wildrift.leagueoflegends.com' + $(this).attr('href')
       news.push(bubble)
     })
-
-    if (news.length === 0) {
-      console.error('❌ 目前沒有抓到任何新聞，可能是選擇器錯誤或網站改版！')
-    }
-
     const reply = {
       type: 'flex',
-      altText: '最新遊戲更新',
+      altText: '最新消息',
       contents: {
         type: 'carousel',
         contents: news
       }
     }
-
-    await event.reply(reply)
+    event.reply(reply)
     writejson(reply, 'news')
   } catch (error) {
-    console.error('取得最新消息失敗：', error)
+    console.error(error)
   }
 }
