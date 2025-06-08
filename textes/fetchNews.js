0a8243d9ff1600d6bc9b8e0cd0ea3c01000c885e
ChiaLingHsu <h200022923@gmail.com>
2025-06-08 14:06:51 +0800

    Update fetchNews.js

--- textes/fetchNews.js
+++ textes/fetchNews.js
@@ -11,11 +11,12 @@ export default async event => {
 
     for (let i = 0; i < Math.min(newsItems.length, 12); i++) {
       const item = newsItems[i]
-      const bubble = JSON.parse(JSON.stringify(templates))
+      const bubble = JSON.parse(JSON.stringify(templates)) // 深拷貝
 
+      // 正確填入資料
       bubble.hero.url = item.heroImage.url
       bubble.body.contents[0].text = item.title
-      bubble.body.contents[1].contents[0].contents[0].text = '日期：' + item.publishDate
+      bubble.body.contents[1].text = '日期：' + item.publishDate
       bubble.footer.contents[0].action.uri = 'https://wildrift.leagueoflegends.com' + item.url.alias
 
       news.push(bubble)
@@ -26,13 +27,14 @@ export default async event => {
       altText: '最新消息',
       contents: {
         type: 'carousel',
-        contents: News
+        contents: news // <== 修正這裡
       }
     }
 
     await event.reply(reply)
-    writejson(reply, 'News')
+    writejson(reply, 'news')
   } catch (error) {
     console.error(error)
+    await event.reply('取得最新消息失敗，請稍後再試。')
   }
 }
