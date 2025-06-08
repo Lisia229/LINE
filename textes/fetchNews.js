b91c1d9a92ff0aee9d5275b65535ba7e4fdbbf44
ChiaLingHsu <h200022923@gmail.com>
2025-06-08 14:20:00 +0800

    Update fetchNews.js

--- textes/fetchNews.js
+++ textes/fetchNews.js
@@ -9,23 +9,20 @@ export default async event => {
     const $ = cheerio.load(data)
     const news = []
 
-    // 抓取每一則新聞卡片 <a>
+    // 抓每一個新聞卡片 <a>
     $('.sc-362cdf8e-0.hSAVYW a.action').each(function (i) {
-      if (i >= 12) return false
-
-      const bubble = JSON.parse(JSON.stringify(templates))
+      if (i >= 12) return false // 最多 12 則
 
       const $card = $(this)
       const image = $card.find('img').attr('src')
       const title = $card.find('[data-testid="card-title"]').text().trim()
       const date = $card.find('[data-testid="card-date"] time').text().trim()
-      const desc = $card.find('[data-testid="card-description"]').text().trim()
       const href = $card.attr('href')
 
+      const bubble = JSON.parse(JSON.stringify(templates))
       bubble.hero.url = image
       bubble.body.contents[0].text = title
-      bubble.body.contents[1].contents[0].contents[0].text = '日期：' + date
-      bubble.body.contents[1].contents[1].text = desc
+      bubble.body.contents[1].text = '日期：' + date
       bubble.footer.contents[0].action.uri = 'https://wildrift.leagueoflegends.com' + href
 
       news.push(bubble)
